9e47edc7cf9d1ea1a4329206e520d5d53423df5c
farynaa <farynaa@yandex.ru>
2019-10-02 11:09:10 +0300

    init: solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
 
     }
 
     guess() {
-
+        this.lastGuess = Math.round((this.min + this.max) / 2);
+        return this.lastGuess
     }
 
     lower() {
-
+        this.max = this.lastGuess;
     }
 
     greater() {
-
+        this.min = this.lastGuess;
     }
 }
 
